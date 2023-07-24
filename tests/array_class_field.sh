48a83441a6613563d0d5c3a866bc296961de465b
andrey <ninzyago@gmail.com>
2023-07-24 13:04:04 +0300

    Add shecial symbols to constants.sh

--- tests/array_class_field.sh
+++ tests/array_class_field.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-declare -A field
-field["piece,1"]="hello1"
-field["piece,2"]="hello2"
-field["piece,3"]="hello3"
-for ((i=1; i<=3; i++))
-do
-    index="piece,${i}"
-    # echo $index
-    echo "${field[$index]}"
+source constants.sh
+
+for symbol in "${SPECIAL_SYMBOLS[@]}"
+do 
+    echo "$symbol"
+done
+
+for descr in "${SPECIAL_SYMBOLS_DESCRIPTION[@]}"
+do 
+    echo "$descr"
 done
